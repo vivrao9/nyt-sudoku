4c8b5d1e9bdfbaeaf788bd03ce4362bb31d9d7d6
Vivek Rao <vivrao9@gmail.com>
2024-01-30 08:31:30 -0500

    set up scrollytelling. next steps: confirm story for scrollytelling, fix radio toggles, banner?

--- data-visualization/nyt-sudoku-viz/src/Grid/Grid.js
+++ data-visualization/nyt-sudoku-viz/src/Grid/Grid.js
@@ -1,45 +1,23 @@
 import styles from './Grid.module.css'
 import '../index.css'
 import { range } from '../utils.js'
-import { scaleQuantize, extent, select, color } from 'd3'
+import { scaleQuantize, scaleDiverging, extent, select, color } from 'd3'
 import { useEffect, useState, useRef } from 'react'
 import { useHover } from '@uidotdev/usehooks'
 
-// prefilledData = {
-//     'easy': [148, 135, 139, 138, 138, 130, 127, 131, 122, 138, 125, 130, 116,
-//         120, 127, 128, 118, 115, 137, 101, 116, 123, 116, 120, 114, 131,
-//         111, 117, 125, 105, 120, 134, 107, 122, 114, 117, 110, 111, 119,
-//         122, 131, 114, 100, 119, 109, 121, 109, 112, 102, 103, 105, 115,
-//         104,  99, 119, 120, 109, 107, 115, 115, 105,  98, 121, 102, 115,
-//         128, 103,  90, 109, 115,  98,  96, 107, 104, 104, 107, 102,  99,
-//         111,  94,  95],
-
-//     'medium': 
-        
-// }
-
-
 // =======================================================================
 // start with Cell component
-function Cell({ index, cellColor, cellValue }) {
+function Cell({ index, cellColor, cellValue, colors }) {
 
     // use hover hook
     const [ref, hovering] = useHover()
-
-    // create hover hook
-    // useEffect(() => {
-    //   const hoveredOnCell
-    // })
-
-//     // set state/style for hovered-on cell
-//     const [ hoveredStyle, setHoveredStyle ] = useState('none')
 
     // create state to track what cell is currently being hovered on
     const [ hoveredOnCell, setHoveredOnCell ] = useState(null)
 
     // set cell style
     const cellStyle = { backgroundColor: cellColor || 'none',
-                        color: cellColor === '#9B8500' ? 'white' : 'black'} // change color of text for color contrast
+                        color: cellColor === colors.slice(-1) ? 'white' : 'black'} // change color of text for color contrast
     
     return <div className={`${styles.grid__cell}`} // ${listOfNeighbors.includes({index}) ? 'neighbor' : ''}`}
                 key={index} // key or else React will yell at us
@@ -51,59 +29,43 @@ function Cell({ index, cellColor, cellValue }) {
                 >
                     <p style={{display: hovering ? 'flex' : 'none',
                                margin: 'auto'}}>
-                        {cellValue}
+                        {Math.round(cellValue * 10) / 10}
                     </p>
             </div>
 }
 
 // ======================================================================================
 // create Grid component
-function Grid({ heatmap = range(81) }) {
-
-    function findNeighbors(index)    {
-        // given a cell, find the (up to) eight neighboring cells
-        // and return their indices
-        const gridSize = 9;
-        const row = Math.floor(index / gridSize);
-        const col = index % gridSize;
-      
-        const neighbors = [];
-      
-        for (let i = Math.max(0, row - 1); i <= Math.min(gridSize - 1, row + 1); i++) {
-            for (let j = Math.max(0, col - 1); j <= Math.min(gridSize - 1, col + 1); j++) {
-              const neighborIndex = i * gridSize + j;
-              if (neighborIndex !== index && neighborIndex >= 0 && neighborIndex < gridSize * gridSize) {
-                neighbors.push(neighborIndex);
-              }
-            }
-          }
-      
-        return neighbors;
-      }
+function Grid({ colorData, colorDiverging=false, colors, text=null, hatchData=null }) {
+  console.log(colorData)
+  console.log(colors)
 
     // convert string of list to list
-    heatmap = JSON.parse(heatmap)
-    
-    // convert raw numbers of percentages
-    heatmap = heatmap.map(item => item)    
-    
+    // heatmap = JSON.parse(heatmap)
+
     // create a D3 scales from min to max of the values
     // in the heatmap array argument
-    var heatmap_scale = scaleQuantize()
-                        .domain(extent(heatmap))
-                        // .range(["#FBC990", "#FFAA4C", "#FA8400", "#C56800", "#854B0A"]) // oranges
-                        .range(['#FFF3B0', '#FFEB75', '#FFDA00', '#C5A900', '#9B8500']) // yellows
+    if (colorDiverging === true) {
+      var heatmap_scale = scaleDiverging()
+                        .domain(extent(colorData))
+                        .range(colors)
+    } else {
+      var heatmap_scale = scaleQuantize()
+                        .domain(extent(colorData))
+                        .range(colors)                
+    }
 
     // console.log(heatmap_scale(heatmap))
-    
+
     return (
         <div className={styles.doks_grid}>
             {range(81).map((item, index) => {
                 return <Cell key={index}
                         index={index}
-                        cellColor={heatmap_scale(heatmap[index])}
-                        cellValue={heatmap[index]}
-                        className={`${styles.grid__cell}`} />
+                        cellColor={heatmap_scale(colorData[index])}
+                        cellValue={colorData[index]}
+                        className={`${styles.grid__cell}`}
+                        colors={colors} />
 })}
         </div>
     )
--- data-visualization/nyt-sudoku-viz/src/ScrollySingles/ScrollySingles.js
+++ data-visualization/nyt-sudoku-viz/src/ScrollySingles/ScrollySingles.js
@@ -2,61 +2,116 @@ import React, { useState } from 'react';
 import { Scrollama, Step } from 'react-scrollama';
 import Grid from '../Grid/Grid';
 import styles from './ScrollySingles.module.css'
+import { range } from '../utils';
+
+const scrollyData = {
+  0:  {
+    colorData: [ 9.45, 10.35, 12.73, 13.32,  9.83,  8.79,  9.5 ,  9.03,  9.65,
+      9.  , 13.45,  8.86,  9.46, 12.78, 12.87,  9.48,  9.12, 11.28,
+     10.08, 11.4 , 11.23,  9.68, 11.  , 13.43, 10.63, 16.94, 12.07,
+      9.88, 13.  , 12.78, 14.26, 10.  , 12.21, 12.16, 15.7 , 11.26,
+     11.46, 12.57, 12.41,  9.38, 12.27, 11.86, 11.46, 14.75,  8.85,
+     12.52, 12.81,  9.72, 15.53, 12.17, 15.1 , 12.  ,  8.95,  9.62,
+     13.84, 12.68, 15.88, 10.65, 15.58, 13.14, 10.57, 10.14, 11.81,
+      9.87,  9.42, 17.94, 10.17, 15.  ,  6.38, 11.43, 12.34, 15.83,
+      9.4 , 14.3 , 10.14, 12.39, 17.71, 14.93, 14.52,  9.69,  9.7 ],
+    colors: ['#EEAFF5', '#F37FFF', '#E40EFA', '#B206C4', '#8A0497'],
+    text: "Once a cell becomes a naked single, on average, it takes me 11.79 steps to fill it out.<br><br>But not all cells are created equally."
+  },
+
+  1:  {
+    colorData: [-2.35, -1.45,  0.93,  1.52, -1.97, -3.01, -2.3 , -2.77, -2.15,
+      -2.8 ,  1.65, -2.94, -2.34,  0.98,  1.07, -2.32, -2.68, -0.52,
+      -1.72, -0.4 , -0.57, -2.12, -0.8 ,  1.63, -1.17,  5.14,  0.27,
+      -1.92,  1.2 ,  0.98,  2.46, -1.8 ,  0.41,  0.36,  3.9 , -0.54,
+      -0.34,  0.77,  0.61, -2.42,  0.47,  0.06, -0.34,  2.95, -2.95,
+       0.72,  1.01, -2.08,  3.73,  0.37,  3.3 ,  0.2 , -2.85, -2.18,
+       2.04,  0.88,  4.08, -1.15,  3.78,  1.34, -1.23, -1.66,  0.01,
+      -1.93, -2.38,  6.14, -1.63,  3.2 , -5.42, -0.37,  0.54,  4.03,
+      -2.4 ,  2.5 , -1.66,  0.59,  5.91,  3.13,  2.72, -2.11, -2.1 ],
+    colorDiverging: false,
+    colors: ['#FFF3B0', '#FFEB75', '#D3D3D3', '#B206C4', '#8A0497'],
+    text: "I take longer to fill out naked singles that are toward the bottom of the puzzle. This might mean I pay less attention to this part of the grid."
+  },
+
+  2:  {
+    colorData: [-2.35, -1.45,  0.93,  1.52, -1.97, -3.01, -2.3 , -2.77, -2.15,
+      -2.8 ,  1.65, -2.94, -2.34,  0.98,  1.07, -2.32, -2.68, -0.52,
+      -1.72, -0.4 , -0.57, -2.12, -0.8 ,  1.63, -1.17,  5.14,  0.27,
+      -1.92,  1.2 ,  0.98,  2.46, -1.8 ,  0.41,  0.36,  3.9 , -0.54,
+      -0.34,  0.77,  0.61, -2.42,  0.47,  0.06, -0.34,  2.95, -2.95,
+       0.72,  1.01, -2.08,  3.73,  0.37,  3.3 ,  0.2 , -2.85, -2.18,
+       2.04,  0.88,  4.08, -1.15,  3.78,  1.34, -1.23, -1.66,  0.01,
+      -1.93, -2.38,  6.14, -1.63,  3.2 , -5.42, -0.37,  0.54,  4.03,
+      -2.4 ,  2.5 , -1.66,  0.59,  5.91,  3.13,  2.72, -2.11, -2.1 ],
+    colorDiverging: false,
+    colors: ['#FFF3B0', '#FFEB75', '#D3D3D3', '#B206C4', '#8A0497'],
+    text: "This could also mean I’m caught up talking to other naked singles before I get to the ones at the bottom.<br><br>Let’s reanalyze these cells using a different data point: average number of seconds to fill a cell."
+  },
+
+  3:  {
+    colorData: [3.43, 4.17, 4.89, 3.75, 4.45, 3.93, 3.46, 3.61, 3.97, 4.35, 5.21,
+      4.72, 3.62, 3.56, 3.82, 3.58, 5.09, 4.29, 3.5 , 4.67, 4.39, 4.79,
+      4.21, 4.19, 4.94, 4.6 , 4.98, 5.04, 4.03, 4.03, 5.39, 5.1 , 3.25,
+      4.05, 3.96, 7.12, 4.05, 5.37, 4.9 , 4.93, 5.62, 5.38, 3.4 , 4.32,
+      4.6 , 5.61, 5.52, 4.44, 5.16, 4.67, 5.29, 3.81, 4.69, 4.12, 3.86,
+      3.59, 4.93, 4.84, 4.83, 3.9 , 4.17, 4.31, 5.93, 4.9 , 4.66, 4.87,
+      5.33, 5.11, 3.79, 4.12, 4.18, 3.88, 4.35, 4.76, 4.48, 5.27, 4.78,
+      5.51, 5.17, 4.23, 4.43],
+    colors: ['#FBC990', '#FFAA4C', '#FA8400', '#C56800', '#854B0A'],
+    text: "On average, I spend about four and a half seconds before I fill out a cell.<br><br>But again, not all cells are created equally."
+  },
+
+  4:  {
+    colorData: [-1.09, -0.35,  0.37, -0.77, -0.07, -0.59, -1.06, -0.91, -0.55,
+      -0.17,  0.69,  0.2 , -0.9 , -0.96, -0.7 , -0.94,  0.57, -0.23,
+      -1.02,  0.15, -0.13,  0.27, -0.31, -0.33,  0.42,  0.08,  0.46,
+       0.52, -0.49, -0.49,  0.87,  0.58, -1.27, -0.47, -0.56,  2.6 ,
+      -0.47,  0.85,  0.38,  0.41,  1.1 ,  0.86, -1.12, -0.2 ,  0.08,
+       1.09,  1.  , -0.08,  0.64,  0.15,  0.77, -0.71,  0.17, -0.4 ,
+      -0.66, -0.93,  0.41,  0.32,  0.31, -0.62, -0.35, -0.21,  1.41,
+       0.38,  0.14,  0.35,  0.81,  0.59, -0.73, -0.4 , -0.34, -0.64,
+      -0.17,  0.24, -0.04,  0.75,  0.26,  0.99,  0.65, -0.29, -0.09],
+    colors: ['#A5E1F8', '#7ED2F2', '#D3D3D3', '#854B0A'],
+    text: "Here's a look at my time per cell and how it differs from the average time per cell."
+  },
+
+  5:  {
+    colorData: [-1.09, -0.35,  0.37, -0.77, -0.07, -0.59, -1.06, -0.91, -0.55,
+      -0.17,  0.69,  0.2 , -0.9 , -0.96, -0.7 , -0.94,  0.57, -0.23,
+      -1.02,  0.15, -0.13,  0.27, -0.31, -0.33,  0.42,  0.08,  0.46,
+       0.52, -0.49, -0.49,  0.87,  0.58, -1.27, -0.47, -0.56,  2.6 ,
+      -0.47,  0.85,  0.38,  5.41,  1.1 ,  0.86, -1.12, -0.2 ,  0.08,
+       1.09,  1.  , -0.08,  0.64,  0.15,  0.77, -0.71,  0.17, -0.4 ,
+      -0.66, -0.93,  0.41,  0.32,  0.31, -0.62, -0.35, -0.21,  1.41,
+       0.38,  0.14,  0.35,  0.81,  0.59, -0.73, -0.4 , -0.34, -0.64,
+      -0.17,  0.24, -0.04,  0.75,  0.26,  0.99,  0.65, -0.29, -0.09],
+    colors: ['#A5E1F8', '#7ED2F2', '#D3D3D3', '#C56800', '#854B0A'],
+    text: "Now let's overlay this with data on steps."
+  },
+}
 
 const ScrollamaDemo = () => {
-  const [currentStepIndex, setCurrentStepIndex] = useState(null);
+  const [currentStepIndex, setCurrentStepIndex] = useState(0)
 
   // This callback fires when a Step hits the offset threshold. It receives the
   // data prop of the step, which in this demo stores the index of the step.
   const onStepEnter = ({ data }) => {
-    setCurrentStepIndex(data);
-  };
+    setCurrentStepIndex(data)
+    console.log(data)
+  }
 
   return (
     <div className={styles.entireScrolly}>
       <figure>
-        <Grid heatmap='[148, 135, 139, 138, 138, 130, 127, 131, 122, 138, 125, 130, 116,
-              120, 127, 128, 118, 115, 137, 101, 116, 123, 116, 120, 114, 131,
-              111, 117, 125, 105, 120, 134, 107, 122, 114, 117, 110, 111, 119,
-              122, 131, 114, 100, 119, 109, 121, 109, 112, 102, 103, 105, 115,
-              104,  99, 119, 120, 109, 107, 115, 115, 105,  98, 121, 102, 115,
-              128, 103,  90, 109, 115,  98,  96, 107, 104, 104, 107, 102,  99,
-              111,  94,  95]' />
+        <Grid {...scrollyData[currentStepIndex]} />
       </figure>
 
       <article>
-        <Scrollama offset={0.9} onStepEnter={onStepEnter} debug>
-          <Step data={1} key={1}>
-            <div className={styles.scrollyCard}>
-              <p>After all other options are eliminated, on average, it takes me xx steps to notice and fill out a cell. In other words, <b>it’s a proxy for how closely I’m paying attention to that cell or area</b>.</p>
-            </div>
-          </Step>
-
-          <Step data={2} key={2}>
-            <div className={styles.scrollyCard}>
-              <p>But not all cells are created equally.</p>
-              <p>When comparing each individual cell with the average number of steps it remained a naked single, it’s clear that I take longer to fill out naked singles in the center of the grid.</p>
-            </div>
-          </Step>
-
-          <Step data={3} key={3}>
-            <div className={styles.scrollyCard}>
-              <p>This doesn’t mean I focus less on this section of the grid. It could mean that I’m caught up filling other naked singles before I get to the ones in the middle.</p>
-              <p>Let’s reanalyze these cells using a different data point: average number of seconds to fill a cell.</p>
-            </div>
-          </Step>
-
-          <Step data={4} key={4}>
-            <div className={styles.scrollyCard}>
-              <p>On average, I spend about four and a half seconds before I fill out a cell. But again, not all cells are created equally.</p>
-            </div>
-          </Step>
-
-          <Step data={5} key={5}>
-            <div className={styles.scrollyCard}>
-              <p>When comparing the time per cell to the average time per cell, it looks like I spend less time on the right hand side of the puzzle. For cells that are in the bottom left and center, I tend to spend longer.</p>
-            </div>
-          </Step>
+        <Scrollama offset={0.5} onStepEnter={onStepEnter}>
+          {range(Object.keys(scrollyData).length).map((item, index) => {
+            return <Step data={index} key={index}><div className={styles.scrollyCard}><p dangerouslySetInnerHTML={{ __html: scrollyData[index]['text'] }}></p></div></Step>
+          })}
         </Scrollama>
       </article>
     </div>
